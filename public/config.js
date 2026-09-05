1b59f94d95f479241ad1a70c27957934933ee69d
peakpeakpeakest <errorblot25@gmail.com>
2026-09-05 23:27:37 +0300

    .

---
+++ public/config.js
@@ -0,0 +1,20 @@
+function getPart2() {
+    var a = 'h';
+    var b = 't';
+    var c = 't';
+    var d = 'p';
+    var e = 's';
+    var f = ':';
+    var g = '/';
+    var h = '/';
+    var i = 'y';
+    var j = 'o';
+    var k = 'u';
+    var l = 't';
+    var m = 'u';
+    var n = '.';
+    var o = 'b';
+    var p = 'e';
+    var q = '/';
+    return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q;
+}
